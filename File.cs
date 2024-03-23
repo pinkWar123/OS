da42bdf43b4f2487a2d381997b212612ff4a71d4
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-03-23 15:59:25 +0700

    Merge code successfully

--- File.cs
+++ File.cs
@@ -22,6 +22,24 @@ class FATFile : FATFileManager
             Console.Write("\t");
         Console.WriteLine("**" + MainName + "." + ExtendedName + "--" + FileSize + "--" + Creationdatetime.Day + "/" + Creationdatetime.Month + "/" + Creationdatetime.Year + "-" + Creationdatetime.Hour + ":" + Creationdatetime.Minute + ":" + Creationdatetime.Second);
     }
+
+    public override int GetSize() { return (int)FileSize; }
+
     ~FATFile() { }
 
+
+    // Methods for UI
+    public override void Populate()
+    {
+        CurrentNode.ImageKey = "fileIcon";
+        CurrentNode.SelectedImageKey = "fileIcon";
+        CurrentNode.Tag = this;
+        CurrentItem.Tag = this;
+        CurrentItem.Text = this.MainName;
+        CurrentItem.SubItems.Add("File");
+        CurrentItem.SubItems.Add(GetSize().ToString());
+        CurrentItem.SubItems.Add(Creationdatetime.ToString());
+        
+
+    }
 }
